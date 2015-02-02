c518725c3714f8ad743a757bd366443e1a9008c3
Loay Gewily <loay@users.noreply.github.com>
2015-02-01 20:21:07 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'INSERT CONSUMER_KEY');
+define('CONSUMER_SECRET', 'INSERT CONSUMER_SECRET');
+define('OAUTH_CALLBACK', 'INSERT OUATH_CALLBACK');
+?>
